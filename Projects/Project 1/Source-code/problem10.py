aecfd6e576516358f3ce10a0e46f7c098efe5d3a
Rogumann <lasse.pladsen@hotmail.com>
2024-02-28 16:02:10 +0100

    progress (copium)

--- Projects/Project 1/Source-code/problem10.py
+++ Projects/Project 1/Source-code/problem10.py
@@ -8,13 +8,13 @@ from problem9 import density_parameters, eos_parameter
 from expressions import eos_integral_N
 
 # Parameters
-z_i = 2e7  # initia redshift 
-z_f = 0    # final redshift
-n = 100    # number of points
-z = np.linspace(z_i, z_f, n)
+z_i = 2e7  # initia redshift
+z_f = 0  # final redshift
+n = 100  # number of points
+# z = np.linspace(z_i, z_f, n)
 
 
-def hubble_parameter(z: float, V: str) -> float:
+def hubble_parameter(V: str) -> float:
     """Returns the characteristic Hubble parameter H/H_0 as a function of the redshift z (eq. 7)
 
     arguments:
@@ -25,21 +25,34 @@ def hubble_parameter(z: float, V: str) -> float:
         the value of H/H_0 for the given redshift z
     """
     # Density parameters
-    _, Omega_m, Omega_r, Omega_phi = density_parameters(V)
+    z, Omega_m, Omega_r, Omega_phi = density_parameters(V)
     Omega_m0 = Omega_m[-1]
     Omega_r0 = Omega_r[-1]
     Omega_phi0 = Omega_phi[-1]
 
+
+    # Eos parameter
+    # z, omega_phi = eos_parameter(V)
+
+    print(np.shape(Omega_m0), np.shape(z))
+
     # Convert redshift z to characteristic time N
     N = np.log(1 / (1 + z))
 
-    return np.sqrt(
+    # return np.sqrt(
+    #     Omega_m0 * (1 + z) ** 3
+    #     + Omega_r0 * (1 + z) ** 4
+    #     + Omega_phi0 * np.exp(quad(eos_integral_N, N, 0)[0])
+    # )
+    # print(Omega_m0 * (1 + z) ** 3)
+    return z, np.sqrt(
         Omega_m0 * (1 + z) ** 3
-        + Omega_r0 * (1 + z) ** 4 + Omega_phi0 * np.exp(quad(eos_integral_N, N, 0)[0])
+        + Omega_r0 * (1 + z) ** 4
+        + Omega_phi0 * np.exp(cumulative_trapezoid(N))
     )
 
 
-def plot_hubble_parameter(V: str, filename=None, figsize=True, prnt=True) -> None:
+def plot_hubble_parameter(V: str, filename=None, figsize=(9, 5), prnt=True) -> None:
     """Plots the characteristic Hubble parameter H/H_0 as a function of the redshift z
 
     arguments:
@@ -47,7 +60,7 @@ def plot_hubble_parameter(V: str, filename=None, figsize=True, prnt=True) -> Non
         filename: the name of the file to save the plot
         figsize: the size of the figure
         prnt: if True, print the value of H/H_0 at z=0
-    
+
     returns:
         None
     """
@@ -62,22 +75,22 @@ def plot_hubble_parameter(V: str, filename=None, figsize=True, prnt=True) -> Non
             )
         )
 
-    y = []
-    for zi in z:
-        y.append(hubble_parameter(zi, V))
+    z, H = hubble_parameter(V)
 
-    print(z[-1], y[-1])
-
-    plt.figure()
+    plt.figure(figsize=figsize)
     plt.title(f"Hubble parameter for {V}-potential")
-    plt.plot(z, y)
-    plt.legend()
+    plt.plot(z, H)
     plt.xscale("log")
     plt.yscale("log")
     plt.xlabel("z")
     plt.ylabel("$H/H_0$")
     plt.grid()
-    plt.show()
+    plt.savefig(filename)
+
+    if prnt:
+        print(z[-1], H[-1])
+        print(z[0], H[0])
+
 
 V = "power"
 
