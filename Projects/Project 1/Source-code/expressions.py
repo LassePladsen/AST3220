c6134b9891994d3dbb3fc726d680894c8dbf9d34
Rogumann <lasse.pladsen@hotmail.com>
2024-02-29 12:45:50 +0100

    removed unused potential functions

--- Projects/Project 1/Source-code/expressions.py
+++ Projects/Project 1/Source-code/expressions.py
@@ -7,52 +7,6 @@ alpha = 1
 xi = 3 / 2
 
 
-def V_power_law(phi: float, M=float) -> float:
-    """Expression 23 of the project
-
-    arguments:
-        phi: field parameter
-        M: mass scale
-
-    returns:
-        the value of the potential
-    """
-    alpha = 1
-    return M ** (4 + alpha) * phi ** (-alpha)
-
-
-def V_exponential(phi: float, V0: float) -> float:
-    """Expression 24 of the project.
-
-    arguments:
-        phi: field parameter
-        V0: the potential constant
-
-    returns:
-        the value of the potential
-    """
-    kappa = np.sqrt(8 * np.pi * G)
-    return V0 * np.exp(-kappa * xi * phi)
-
-
-def potential(V: str) -> Callable:
-    """Returns the correct potential function
-
-    arguments:
-        V: the potential function in ["power", "exponential"]
-
-    returns:
-        the potential function
-    """
-
-    if V.lower() == "power":
-        return V_power_law
-    elif V.lower() == "exponential":
-        return V_exponential
-    else:
-        raise ValueError("V-string value not recognized")
-
-
 def gamma(V: str) -> float:
     """Describes Gamma(phi), equation 18 of the project, which depends on
     the potential
