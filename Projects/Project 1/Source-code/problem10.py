e1aaa54b41676e1b07fdc9459d7b95002932fa34
Rogumann <lasse.pladsen@hotmail.com>
2024-02-29 11:29:50 +0100

    reverted to using integrate quad

--- Projects/Project 1/Source-code/problem10.py
+++ Projects/Project 1/Source-code/problem10.py
@@ -2,16 +2,10 @@ import os
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import quad, cumulative_trapezoid
+from scipy.integrate import quad
 
-from problem9 import density_parameters, eos_parameter
+from problem9 import density_parameters
 from expressions import eos_integral_N
-
-# # Parameters
-# z_i = 2e7  # initia redshift
-# z_f = 0  # final redshift
-# n = 100  # number of points
-# # z = np.linspace(z_i, z_f, n)
 
 
 def hubble_parameter(V: str) -> tuple[np.ndarray, np.ndarray]:
@@ -21,7 +15,8 @@ def hubble_parameter(V: str) -> tuple[np.ndarray, np.ndarray]:
         V: the potential function in ["power", "exponential"]
 
     returns:
-        the values of H/H_0 for each z from the z-interval in problem 9
+        z: the redshift z
+        H: the values of H/H_0 for each z from the z-interval
     """
     # Density parameters
     z, Omega_m, Omega_r, Omega_phi = density_parameters(V)
@@ -29,35 +24,18 @@ def hubble_parameter(V: str) -> tuple[np.ndarray, np.ndarray]:
     Omega_r0 = Omega_r[-1]
     Omega_phi0 = Omega_phi[-1]
 
-    # Eos parameter
-    _, omega_phi = eos_parameter(V)
-
-    # print(np.shape(Omega_m0), np.shape(z))
-
     # Convert redshift z to characteristic time N
     N = np.log(1 / (1 + z))
-    # N = np.flip(np.log(1 / (1 + z)))
 
-    # print(z)
-    # print(N)
-    # print(omega_phi)
-    # quit()
+    H = np.empty_like(z)
 
-    # H = np.sqrt(
-    #     Omega_m0 * (1 + z) ** 3
-    #     + Omega_r0 * (1 + z) ** 4
-    #     + Omega_phi0 * np.exp(quad(eos_integral_N, N, 0)[0])
-    # )
+    for i, zi in enumerate(z):
+        H[i] = np.sqrt(
+            Omega_m0 * (1 + zi) ** 3
+            + Omega_r0 * (1 + zi) ** 4
+            + Omega_phi0 * np.exp(quad(eos_integral_N, N[i], 0)[0])
+        )
 
-    # print(Omega_m0 * (1 + z) ** 3)
-    H = np.sqrt(
-        Omega_m0 * (1 + z) ** 3
-        + Omega_r0 * (1 + z) ** 4
-        + Omega_phi0
-        # * np.exp(cumulative_trapezoid(eos_integral_N(N), N, initial=0))
-        # * np.exp(cumulative_trapezoid(eos_integral_N(omega_phi), N, initial=0))
-        * np.exp(cumulative_trapezoid(eos_integral_N(omega_phi), omega_phi, initial=0))
-    )
     return z, H
 
 
@@ -104,12 +82,6 @@ def plot_hubble_parameter(
         print(z[0], H[0])
 
 
-V = "power"
-
-# z, omega_phi = eos_parameter(V)
-# N = np.log(1/(1+z))
-# print(N)
-# print(quad(eos_integral_N, N[], 0)[0])
-# print(np.shape(N), np.shape(eos_integral_N(omega_phi)))
-# print(cumulative_trapezoid(N, eos_integral_N(omega_phi)))
-plot_hubble_parameter(V)
+if __name__ == "__main__":
+    for V in ["power", "exponential"]:
+        plot_hubble_parameter(V)
