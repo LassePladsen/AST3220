a1979b198f05b5b1481ae9990fef5aefba34cd5d
Rogumann <lasse.pladsen@hotmail.com>
2024-03-01 13:18:35 +0100

    finished prob11

--- Projects/Project 1/Source-code/problem11.py
+++ Projects/Project 1/Source-code/problem11.py
@@ -1,28 +1,58 @@
 import numpy as np
-from scipy.integrate import quad
+from scipy.integrate import simpson
 
-from expressions import eos_integral_N
-from problem9 import density_parameters
 from problem10 import hubble_parameter_quintessence, hubble_parameter_lambdacdm
 
-V = "power"
-z, Omega_m, Omega_r, Omega_phi = density_parameters(V)
-Omega_m0 = Omega_m[-1]
-Omega_r0 = Omega_r[-1]
-Omega_phi0 = Omega_phi[-1]
-# _, h = hubble_parameter_quintessence("power")
-# h = hubble_parameter_lambdacdm(z)
 
-def hubble(zi):
-    Ni = np.log(1 / (1 + zi))
-    np.sqrt(
-        Omega_m0 * (1 + zi) ** 3
-        + Omega_r0 * (1 + zi) ** 4
-        + Omega_phi0 * np.exp(quad(eos_integral_N, Ni, 0)[0])
+def characteristic_age_quintessence(V: str) -> float:
+    """
+    Calculate the characteristic age of the universe H_0t_0 for the quintessence model
+
+    arguments
+    V: V: the quintessence potential function in ["power", "exponential"]
+
+    returns
+        The characteristic age of the universe
+
+    """
+    z, h = hubble_parameter_quintessence(V)
+    N = np.log(1 / (1 + z))
+    return simpson(h, N)
+
+
+def characteristic_age_lambdacdm() -> float:
+    """
+    Calculate the characteristic age of the universe H_0t_0 for the Lambda-CDM model
+
+    arguments
+        none
+    returns
+        The characteristic age of the universe
+
+    """
+    z = hubble_parameter_quintessence("power")[0]
+    h = hubble_parameter_lambdacdm(z)
+    N = np.log(1 / (1 + z))
+    return simpson(h, N)
+
+
+def print_characteristic_ages() -> None:
+    """Prints the characteristic ages of the universe for two quintessence models
+    and the Lambda-CDM model
+    
+    arguments
+        none
+    
+    returns
+        none
+    """
+    print("Characteristic age of the universe for quintessence models:")
+    for V in ["power", "exponential"]:
+        print(f"{V}-potential: {characteristic_age_quintessence(V):e}")
+    print()
+    print(
+        f"Characteristic age of the universe for Lambda-CDM: {characteristic_age_lambdacdm():e}"
     )
 
-def I(zi):
-    return 1/( (1+zi)*hubble(zi) )
-
-# print(I(z[5]))
-print(quad(I, 0, np.inf))
+if __name__ == "__main__":
+    print_characteristic_ages()
