88e5f35d5c09ea5056e409b7cf8a8710cff0b7b0
Rogumann <lasse.pladsen@hotmail.com>
2024-03-01 13:21:34 +0100

    characterstic -> dimensionless

--- Projects/Project 1/Source-code/problem11.py
+++ Projects/Project 1/Source-code/problem11.py
@@ -4,15 +4,15 @@ from scipy.integrate import simpson
 from problem10 import hubble_parameter_quintessence, hubble_parameter_lambdacdm
 
 
-def characteristic_age_quintessence(V: str) -> float:
+def universe_age_quintessence(V: str) -> float:
     """
-    Calculate the characteristic age of the universe H_0t_0 for the quintessence model
+    Calculate the dimensionless age of the universe H_0t_0 for the quintessence model
 
     arguments
     V: V: the quintessence potential function in ["power", "exponential"]
 
     returns
-        The characteristic age of the universe
+        The dimensionless age of the universe
 
     """
     z, h = hubble_parameter_quintessence(V)
@@ -20,14 +20,14 @@ def characteristic_age_quintessence(V: str) -> float:
     return simpson(h, N)
 
 
-def characteristic_age_lambdacdm() -> float:
+def universe_age_lambdacdm() -> float:
     """
-    Calculate the characteristic age of the universe H_0t_0 for the Lambda-CDM model
+    Calculate the dimensionless age of the universe H_0t_0 for the Lambda-CDM model
 
     arguments
         none
     returns
-        The characteristic age of the universe
+        The dimensionless age of the universe
 
     """
     z = hubble_parameter_quintessence("power")[0]
@@ -36,23 +36,23 @@ def characteristic_age_lambdacdm() -> float:
     return simpson(h, N)
 
 
-def print_characteristic_ages() -> None:
-    """Prints the characteristic ages of the universe for two quintessence models
+def print_universe_ages() -> None:
+    """Prints the dimensionless ages of the universe for two quintessence models
     and the Lambda-CDM model
-    
+
     arguments
         none
-    
+
     returns
         none
     """
-    print("Characteristic age of the universe for quintessence models:")
+    print("Dimensionless age of the universe for quintessence models:")
     for V in ["power", "exponential"]:
-        print(f"{V}-potential: {characteristic_age_quintessence(V):e}")
+        print(f"{V}-potential: {universe_age_quintessence(V):e}")
     print()
     print(
-        f"Characteristic age of the universe for Lambda-CDM: {characteristic_age_lambdacdm():e}"
+        f"Dimensionless age of the universe for Lambda-CDM: {universe_age_lambdacdm():e}"
     )
 
 if __name__ == "__main__":
-    print_characteristic_ages()
+    print_universe_ages()
