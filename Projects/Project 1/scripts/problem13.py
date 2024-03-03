c378b3a6db758ed062603dc446fcf29ac8479803
Rogumann <lasse.pladsen@hotmail.com>
2024-03-03 13:16:56 +0100

    Printed chi-squared values, but they dont look right, need to convert to Gpc

---
+++ Projects/Project 1/scripts/problem13.py
@@ -0,0 +1,53 @@
+import os
+
+import numpy as np
+
+from problem12 import luminosity_distance_quintessence
+
+DATA_PATH = os.path.join(
+    os.path.dirname(__file__), "..", "data", "luminosity_distances.txt"
+)
+
+
+def chi_squared(prediction: np.ndarray, data: np.ndarray, err: np.ndarray) -> float:
+    """
+    Calculate the chi-squared value for a model prediction given the data and the errors
+
+    arguments:
+        prediction: the model prediction [Gpc]
+        data: the observed data [Gpc]
+        err: the errors in the observed data [Gpc]
+
+    returns:
+        The chi-squared value
+    """
+    return np.sum(((prediction - data) / err) ** 2)
+
+
+def print_chi_squared_values() -> None:
+    """Prints the chi-squared values for the two quintessence models
+
+    arguments:
+        none
+
+    returns:
+        none
+    """
+
+    # Load data
+    z, d_L, err = np.loadtxt(DATA_PATH, skiprows=5, unpack=True)
+
+    # Characteristic time interval
+    N_i = np.log(1 / (z[-1] + 1))
+    N_f = np.log(1 / (z[0] + 1))
+
+    # Model predictions
+    for V in ["power", "exponential"]:
+        d_L_model = luminosity_distance_quintessence(V, N_i, N_f, n_points=len(z))[-1]
+        print(
+            f"Chi-squared value for {V}-potential: {chi_squared(d_L_model, d_L, err)}"
+        )
+
+
+if __name__ == "__main__":
+    print_chi_squared_values()
