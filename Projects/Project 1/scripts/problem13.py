e1538e123a6278d860869dc42a5325a2917ecde3
Rogumann <lasse.pladsen@hotmail.com>
2024-03-06 20:38:23 +0100

    ??

--- Projects/Project 1/scripts/problem13.py
+++ Projects/Project 1/scripts/problem13.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from problem12 import luminosity_distance_quintessence
 
@@ -9,7 +10,7 @@ DATA_PATH = os.path.join(
 )
 
 # Constants
-H_0 = 70  # Hubble constant [m/s/Gpc]
+H_0 = 70e6  # Hubble constant [m/s/Gpc]
 c = 3e8  # speed of light [m/s]
 
 
@@ -28,6 +29,56 @@ def chi_squared(prediction: np.ndarray, data: np.ndarray, err: np.ndarray) -> fl
     return np.sum(((prediction - data) / err) ** 2)
 
 
+def plot_luminosity_distances(
+    filename: str = None,
+    figsize: tuple[int, int] = (6, 4),
+) -> None:
+    """Plots the luminosity distance using the data and the quintessence models
+    as a function of the redshift, in Gpc units
+
+    arguments:
+        filename: the filename to save the plot figure
+        figsize: the plot figure size
+
+    returns:
+        none
+    """
+
+    # Default filename
+    if not filename:
+        filename = os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__),
+                "..",
+                "Figures",
+                f"13_luminosity_distances.png",
+            )
+        )
+
+    plt.figure(figsize=figsize)
+
+    # Load data
+    z, d_data, d_err = np.loadtxt(DATA_PATH, skiprows=5, unpack=True)  # [-, Gpc, Gpc]
+    N_i = np.log(1 / (1 + z[-1]))
+    N_f = np.log(1 / (1 + z[0]))
+
+    # Plot the two quintessence models
+    for V in ["power", "exponential"]:
+        zi, d_model = luminosity_distance_quintessence(V, N_i, N_f)
+        plt.plot(zi, d_model * c/H_0, label=V)  # convert to Gpc when plotting
+
+    # Plot the data
+    plt.errorbar(z, d_data, yerr=d_err, fmt=".", label="Data points", color="gray")
+
+    plt.xlabel("$z$")
+    plt.ylabel("$d_L$ [Gpc]")
+    plt.title("Luminosity distance")
+    plt.gca().invert_xaxis()  # invert x-axis
+    plt.legend()
+    plt.grid()
+    plt.savefig(filename)
+
+
 def print_chi_squared_values() -> None:
     """Prints the chi-squared values for the two quintessence models
 
@@ -39,7 +90,7 @@ def print_chi_squared_values() -> None:
     """
 
     # Load data
-    z, d_L, err = np.loadtxt(DATA_PATH, skiprows=5, unpack=True)  # [-, Gpc, Gpc]
+    z, d_data, d_err = np.loadtxt(DATA_PATH, skiprows=5, unpack=True)  # [-, Gpc, Gpc]
 
     # Characteristic time interval
     N_i = np.log(1 / (z[-1] + 1))
@@ -47,14 +98,13 @@ def print_chi_squared_values() -> None:
 
     # Model predictions
     for V in ["power", "exponential"]:
-        d_L_model = luminosity_distance_quintessence(V, N_i, N_f, n_points=len(z))[-1]
-        d_L_model *= c / H_0  # convert to Gpc
-        print(d_L_model)
-        print(d_L)
+        d_model = luminosity_distance_quintessence(V, N_i, N_f, n_points=len(z))[-1]
+        d_model *= c / H_0  # convert to Gpc
         print(
-            f"Chi-squared value for {V}-potential: {chi_squared(d_L_model, d_L, err)}"
+            f"Chi-squared value for {V}-potential: {chi_squared(d_model, d_data, d_err)}"
         )
 
 
 if __name__ == "__main__":
+    plot_luminosity_distances()
     print_chi_squared_values()
