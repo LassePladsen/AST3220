3d407663b111a60578308ff6c8ef784c5568d2d5
Rogumann <lasse.pladsen@hotmail.com>
2024-03-07 13:04:23 +0100

    ..

--- Projects/Project 1/scripts/runall.py
+++ Projects/Project 1/scripts/runall.py
@@ -4,7 +4,7 @@ import os
 if __name__ == "__main__":
     print("Running all project 1 scripts...")
 
-    # Run all scripts
+    # Run all scripts problem 9 to 14
     for nr in range(9, 15):
         print(f"\nProblem {nr}:")
         path = os.path.abspath(
