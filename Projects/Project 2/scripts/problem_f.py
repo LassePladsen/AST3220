874e657d2060f930c67da28ba6cdbfd6629de4e6
Rogumann <lasse.pladsen@hotmail.com>
2024-04-09 17:17:21 +0200

    normal units

--- Projects/Project 2/scripts/problem_f.py
+++ Projects/Project 2/scripts/problem_f.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import os
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,19 +8,20 @@ from scipy.integrate import solve_ivp, quad
 # Variables
 T_i = 100e9  # initial temperature [K]
 T_f = 0.1e9  # final temperature [K]
+n_points = 1000  # number of points in the time array
 tau = 1700  # fee neutron decay time [s]
 N_eff = 3  # effective number of neutrino species
 
-# Constants, in CGS units
+# Constants
 c = 2.9979e10  # speed of light [cm/s]
-k = 8.617e-5  # Boltzmann constant [eV/K]
-hbar = 6.582e-16  # reduced Planck constant [eV*s]
-G = 6.674e-8  # gravitational constant [cm^3/g/s^2]
+k = 1.380649e-23  # Boltzmann constant [J/K]
+hbar = 1.05457182e-34  # reduced Planck constant [J*s]
+G = 6.6743e-11  # gravitational constant [N*m^2/kg^2]
 T_0 = 2.725  # CMB temperature [K]
-m_p = 938.272e6  # proton mass [eV/c^2]
-m_n = 939.565e6  # neutron mass [eV/c^2]
-q = 2.53  # mass difference ratio
-H_0 = 22.686e-19  # Hubble constant [1/s]
+m_p = 1.67262192e-27  # proton mass [kg]
+m_n = 1.6749286e-27  # neutron mass [kg]
+H_0 = 2.26534e-18  # Hubble constant [1/s]
+q = 2.53  # mass difference ratio [(m_n - m_p) / m_e]
 Omega_r0 = (  # radiation density parameter today
     8
     * np.pi**3
@@ -30,6 +32,13 @@ Omega_r0 = (  # radiation density parameter today
     / (hbar**3 * c**5)
     * (1 + N_eff * 7 / 8 * (4 / 11) ** (4 / 3))
 )
+
+
+# Directort to save figures
+FIGURES_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "figures"))
+
+if not os.path.exists(FIGURES_DIR):
+    os.makedirs(FIGURES_DIR)
 
 
 @lru_cache
@@ -45,29 +54,30 @@ def T_nu(T: float) -> float:
     return (4 / 11) ** (1 / 3) * T
 
 
-@lru_cache
-def Y_ni(T: float) -> float:
-    """Initial value of Y_n, equation (16) of the project.
+# @lru_cache
+@np.vectorize
+def Y_n_eq(T: float) -> float:
+    """Thermal equilibrium value of relative number density of neutrons Y_n, equation (16) of the project.
 
     arguments:
         T: temperature [K]
 
     returns:
-        the initial value of Y_n
+        the thermal equilibrium value of Y_n
     """
     return 1 / (1 + np.exp((m_n - m_p) * c * c / (k * T)))
 
 
-def Y_pi(T: float) -> float:
-    """Initial value of Y_p, equation (17) of the project.
+def Y_p_eq(T: float) -> float:
+    """Thermal equilibrium value of relative number density of protons Y_p, equation (17) of the project.
 
     arguments:
         T: temperature [K]
 
     returns:
-        the initial value of Y_n
+        the thermal equilibrium value of Y_n
     """
-    return 1 - Y_ni(T)
+    return 1 - Y_n_eq(T)
 
 
 @lru_cache
@@ -123,32 +133,29 @@ def Gamma_pn(T: float) -> float:
 
 
 @lru_cache
-def a(t: float) -> float:
-    """Scale factor as function of time, result from problem d.
+def a(T: float) -> float:
+    """Scale factor as function of temperature
 
     arguments:
-        t: time [s]
+        T: temperature [K]
 
     returns:
         the scale factor
     """
-    return np.sqrt(
-        2 * H_0 * np.sqrt(Omega_r0) * t + 1
-    )  ############## TODO: not sure if this is correct
+    return T_0 / T
 
 
 @lru_cache
-def H(t: float) -> float:
-    """Hubble parameter as function of time, equation (14) of the project, using
-    a(t) result from problm d.
+def H(T: float) -> float:
+    """Hubble parameter as function of temperature, equation (14) of the project
 
     arguments:
-        t: time [s]
+        t: temperature [K]
 
     returns:
         the Hubble parameter
     """
-    return H_0 * np.sqrt(Omega_r0) * a(t) ** (-2)
+    return H_0 * np.sqrt(Omega_r0) * a(T) ** (-2)
 
 
 def ode_system(lnT: np.ndarray, X: np.ndarray) -> list[float]:
@@ -165,7 +172,96 @@ def ode_system(lnT: np.ndarray, X: np.ndarray) -> list[float]:
 
     Y_n, Y_p = X
 
-    dY_n = -1 / H * (Y_p * Gamma_pn - Y_n * Gamma_np)
-    dY_p = 1 / H * (Y_n * Gamma_np - Y_p * Gamma_pn)
+    dY_n = (
+        -1
+        / H(np.exp(lnT))
+        * (Y_p * Gamma_pn(np.exp(lnT)) - Y_n * Gamma_np(np.exp(lnT)))
+    )
+    dY_p = (
+        -1
+        / H(np.exp(lnT))
+        * (Y_n * Gamma_np(np.exp(lnT)) - Y_p * Gamma_pn(np.exp(lnT)))
+    )
 
     return [dY_n, dY_p]
+
+
+def solve_ode_system() -> tuple[np.ndarray, np.ndarray]:
+    """Solves the ode system of the equations of motion for Y_n and Y_p
+
+    arguments:
+        None
+
+    returns:
+        the time array and the solution array
+    """
+
+    # Parameters
+    lnT_i = np.log(T_i)
+    lnT_f = np.log(T_f)
+
+    # Initial conditions
+    X_i = [Y_n_eq(T_i), Y_p_eq(T_i)]
+
+    # Solve the ODE system
+    tol = 1e-12
+    sol = solve_ivp(
+        ode_system,
+        [lnT_i, lnT_f],
+        X_i,
+        method="Radau",
+        rtol=tol,
+        atol=tol,
+        dense_output=True,
+    )
+
+    t = np.linspace(sol.t[0], sol.t[-1], n_points)
+    return t, sol.sol(t)
+
+
+def plot_relative_number_densities(
+    filename: str = None, figsize: tuple[int, int] = (9, 5)
+) -> None:
+    """Plots the relative number densities of neutrons and protons as a function of logarithmic temperature ln(T)
+
+    arguments:
+        filename: the filename to save the plot figure
+        figsize: the plot figure size
+
+    returns:
+        None
+    """
+
+    # Make default filename
+    if not filename:
+        filename = os.path.join(
+            FIGURES_DIR,
+            f"f_relative_number_densities.png",
+        )
+
+    lnT, X = solve_ode_system()
+    Y_n, Y_p = X
+
+    plt.figure(figsize=figsize)
+
+    # Plot results
+    plt.plot(lnT, Y_n, "r", label="n")
+    plt.plot(lnT, Y_p, "b", label="p")
+
+    # Plot thermal equilibrium values as dotted line
+    # print(Y_n_eq(np.exp(lnT)))
+    plt.plot(lnT, Y_n_eq(np.exp(lnT)), "r--")
+    plt.plot(lnT, Y_p_eq(np.exp(lnT)), "b--")
+
+    # Plot settings
+    plt.xlabel(r"$\ln(T)$")
+    plt.ylabel(r"$Y_i$")
+    plt.legend()
+    plt.grid()
+    plt.title("Relative number densities of neutrons and protons")
+    plt.savefig(filename)
+
+
+if __name__ == "__main__":
+    print(Y_n_eq(np.array([1e5, 1e7, 1e10])))
+    # plot_relative_number_densities()
