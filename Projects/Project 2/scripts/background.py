Fixed bugs and f plot, while converting to using cgs units by standard

Projects/Project 2/scripts/background.py
@@ -1,44 +1,39 @@
 from functools import lru_cache
+from abc import ABC
 
 import numpy as np
+from astropy import units as au, constants as ac
+
+class Constants(ABC):
+    """Abstract base class"""
+    T_0 = 2.725 # CMB temperature [K]
+    H_0 = H_0 = 22.686e-19  # Hubble constant with h=0.7 [1/s]
 
 
-class SI:
+class SI(Constants):
     """Class to hold constants used in the project given in SI units"""
-
-    def __init__(self) -> None:
-        """Initializes the class with the constants'
-        """
-        self.c = 2.9979e8  # speed of light [m/s]
-        self.k = 1.380649e-23  # Boltzmann constant [J/K]
-        self.hbar = 6.62607015e-34  # reduced Planck constant [J*s]
-        self.H_0 = 22.686e-19  # Hubble constant [1/s]
-        self.T_0 = 2.725  # CMB temperature [K]
-        self.G = 6.67430e-11  # gravitational constant [N*m^2/kg^2]
-        self.m_p = 1.67262192369e-27  # proton mass [kg]
-        self.m_n = 1.67492749804e-27  # neutron mass [kg]
+    c = ac.c.value  # speed of light [m/s]
+    k = ac.k_B.value  # Boltzmann constant [J/K]
+    hbar = ac.hbar.value  # reduced Planck constant [J*s]
+    G = ac.G.value  # gravitational constant [N*m^2/kg^2]
+    m_p = ac.m_p.value  # proton mass [kg]
+    m_n = ac.m_n.value  # neutron mass [kg]
 
 
-class CGS:
+class CGS(Constants):
     """Class to hold constants used in the project given in CGS units"""
-
-    def __init__(self) -> None:
-        """Initializes the class with the constants
-        """
-        self.c = 2.9979e10  # speed of light [cm/s]
-        self.k = 1.380649e-23  # Boltzmann constant [J/K]
-        self.hbar = 6.62607015e-34  # reduced Planck constant [J*s]
-        self.H_0 = 22.686e-12  # Hubble constant [1/s]
-        self.T_0 = 2.725  # CMB temperature [K]
-        self.G = 6.67430e-8  # gravitational constant [cm^3/g/s^2]
-        self.m_p = 1.67262192369e-24  # proton mass [g]
-        self.m_n = 1.67492749804e-24  # neutron mass [g]
+    c = ac.c.value  # speed of light [m/s]
+    k = ac.k_B.value  # Boltzmann constant [J/K]
+    hbar = ac.hbar.value  # reduced Planck constant [J*s]
+    G = ac.G.value  # gravitational constant [N*m^2/kg^2]
+    m_p = ac.m_p.value  # proton mass [kg]
+    m_n = ac.m_n.value  # neutron mass [kg]
 
 
 class Background:
     """Class to store the background functions"""
 
-    def __init__(self, unit: str = "SI", N_eff: int = 3) -> None:
+    def __init__(self, unit: str = "CGS", N_eff: int = 3) -> None:
         """Initializes the class with the given parameters.
 
         arguments:
@@ -64,7 +59,6 @@ class Background:
             * (1 + self.N_eff * 7 / 8 * (4 / 11) ** (4 / 3))
         )
 
-    @lru_cache
     def a(self, T: float) -> float:
         """Scale factor as function of temperature
 
@@ -76,7 +70,6 @@ class Background:
         """
         return self.const.T_0 / T
 
-    @lru_cache
     def H(self, T: float) -> float:
         """Hubble parameter as function of temperature, equation (14) of the project
 
@@ -87,3 +80,14 @@ class Background:
             the Hubble parameter
         """
         return self.const.H_0 * np.sqrt(self.Omega_r0) * self.a(T) ** (-2)
+
+
+"""if __name__ == '__main__':
+    # Debug plot H(T)
+    import matplotlib.pyplot as plt
+    b = Background()
+    T = np.logspace(11, 7)
+    h = b.H(T)
+    print(h[0])
+    plt.loglog(T, h)
+    plt.show()"""

Projects/Project 2/scripts/bbn.py
@@ -1,4 +1,5 @@
 import os
+import warnings
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,6 +7,9 @@ from scipy.integrate import solve_ivp
 
 from reaction_rates import ReactionRates
 from background import Background
+
+# Ignore overflow runtimewarning
+warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 
 class BBN:
@@ -193,13 +197,19 @@ class BBN:
         return self.T, self.Y
 
     def plot_relative_number_densities(
-        self, filename: str, figsize: tuple[int, int] = (7, 5)
+        self,
+        filename: str,
+        figsize: tuple[int, int] = (7, 5),
+        ymin: float = 1e-3,
+        ymax: float = 2.0,
     ) -> None:
         """Plots the relative number densities for each species, as a function of logarithmic temperature ln(T)
 
         arguments:
             filename: the filename to save the plot figure
             figsize: the plot figure size
+            ymin: the minimum y-axis value
+            ymax: the maximum y-axis value
 
         returns:
             None
@@ -231,7 +241,8 @@ class BBN:
         plt.gca().invert_xaxis()  # invert x-axis
         plt.xlabel("T [K]")
         plt.ylabel(r"$Y_i$")
-        plt.ylim(bottom=1e-3, top=2)
+
+        plt.ylim(bottom=ymin, top=ymax)
         plt.legend()
         plt.grid()
         plt.title("Relative number densities of particles species")
@@ -243,10 +254,12 @@ if __name__ == "__main__":
     N_species = 2
     T_i = 1e11  # initial temperature [K]
     T_f = 1e8  # final temperature [K]
-    n_points = 1001
+    n_points = 1001  # number of points for plotting
+    N_eff = 3  # effective number of neutrino species
+    unit = "cgs"  # unit system to use
 
     # Initialize
-    bbn = BBN(N_species, N_eff=15)
+    bbn = BBN(N_species, N_eff=N_eff, unit=unit)
 
     # Solve ode
     bbn.solve_ode_system(T_i, T_f, n_points)