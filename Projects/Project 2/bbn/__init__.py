5ec5ba090583045d1d480975a611492462816bc1
Rogumann <lasse.pladsen@hotmail.com>
2024-04-11 16:16:59 +0200

    imported submodules for ease of use

---
+++ Projects/Project 2/bbn/__init__.py
@@ -0,0 +1,11 @@
+"""
+Big bang nucleosynthesis module for Project 2 of UiO AST3320 Cosmology I
+Project 2 link: 
+https://www.uio.no/studier/emner/matnat/astro/AST3220/v24/undervisningsmateriale/prosjekt-2-ast3220-2024.pdf
+"""
+
+# Import submodules
+from bbn import BBN
+from background import Background
+from reaction_rates import ReactionRates
+from constants import SI, CGS
