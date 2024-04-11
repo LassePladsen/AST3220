5763f99fd40d7b7ded98345fb498eb074282aba1
Rogumann <lasse.pladsen@hotmail.com>
2024-04-11 20:02:18 +0200

    progress

---
+++ Projects/Project 2/problem_scripts/problem_i.py
@@ -0,0 +1,25 @@
+"""Script that solves problem h of the project"""
+
+import sys
+import os
+
+# Append path to bbn package
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "bbn")))
+
+from bbn import BBN, FIG_DIR
+
+if __name__ == "__main__":
+    # Variables
+    N_species = 4  # number of interacting atom species
+    T_i = 1e11  # initial temperature [K]
+    T_f = 1e8  # final temperature [K]
+
+    # Initialize
+    bbn = BBN(N_species)
+
+    # Solve ode
+    bbn.solve_ode_system(T_i, T_f)
+
+    # Plot mass fractions
+    filename = os.path.join(FIG_DIR, "i_mass_fractions.png")
+    bbn.plot_mass_fractions(filename)
