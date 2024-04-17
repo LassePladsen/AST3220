bd535558fcb281b1bc1a5263a931d1be1751b0db
Rogumann <lasse.pladsen@hotmail.com>
2024-04-17 15:54:09 +0200

    removed imports, added to docstring

--- Projects/Project 2/bbn/__init__.py
+++ Projects/Project 2/bbn/__init__.py
@@ -2,13 +2,14 @@
 Big bang nucleosynthesis module for Project 2 of UiO AST3320 Cosmology I
 Project 2 link: 
 https://www.uio.no/studier/emner/matnat/astro/AST3220/v24/undervisningsmateriale/prosjekt-2-ast3220-2024.pdf
+
+Classes:
+bbn.BBN
+background.Background
+reaction_rates.ReactionRates
+constants.SI
+constants.CGS
+
+Variables:
+FIG_DIR (directory of stored figures)
 """
-
-# Import submodules
-from bbn import BBN
-from background import Background
-from reaction_rates import ReactionRates
-from constants import SI, CGS
-
-# Variable imported from bbn submodule:
-# FIG_DIR (directory of stored figures)
