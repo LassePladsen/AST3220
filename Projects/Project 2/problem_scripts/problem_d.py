7f8d7ba961f5827a7199efd746ddf467a16417b3
Rogumann <lasse.pladsen@hotmail.com>
2024-04-17 15:58:19 +0200

    added problem d

---
+++ Projects/Project 2/problem_scripts/problem_d.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+import numpy as np
+
+# Append path to bbn package
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from bbn import constants
+
+if __name__ == "__main__":
+    u = constants.SI()
+    H0sqrtOmega_r0 = np.sqrt(  # H_0 * sqrt(Omega_r0)
+        8
+        * np.pi**3
+        / 45
+        * u.G
+        * (u.k * u.T_0) ** 4
+        / (u.hbar**3 * u.c**5)
+        * (1 + 3 * 7 / 8 * (4 / 11) ** (4 / 3))
+    )
+    print(f"H_0 * sqrt(Omega_r0): {H0sqrtOmega_r0:.4e}")
+    for T in [1e10, 1e9, 1e8]:
+        t = 1 / (2 * H0sqrtOmega_r0) * (u.T_0 / T) ** 2
+        print(f"Time at T = {T:.4e}: {t:.4e} s")
