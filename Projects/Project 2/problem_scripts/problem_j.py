made the final subplot, but it shouldnt be zero? Probably cause wrong values

Projects/Project 2/problem_scripts/problem_j.py
@@ -49,7 +49,7 @@ def bayesian_probability(
     """
     return (
         1
-        / (np.sqrt(2 * error ** (2 * len(predicted))))
+        / (np.sqrt(2 * np.prod(error * error)))
         * np.exp(-xi_squared(predicted, observed, error))
     )
 
@@ -69,7 +69,7 @@ def interpolate_Y_func_of_Omegab0(
         Tuple containing the logspace interpolated mass fractions functions logY_i(logOmega_b0)
         for i = D, He3, He4, and Li7
     """
-    Y_model = np.zeros((4, len(Omega_b0_vals)))
+    Y_model = np.zeros((5, len(Omega_b0_vals)))
     for i, Omega_b0 in enumerate(Omega_b0_vals):
         # Initialize
         bbn = BBN(N_species, Omega_b0=Omega_b0)
@@ -90,42 +90,45 @@ def interpolate_Y_func_of_Omegab0(
         # Extract mass fractions
         Y_p = Y[1]
         Y_D = Y[2]
+        Y_He3 = Y[4]
         Y_He4 = Y[5]
         Y_Li7 = Y[6]
-        Y = np.asarray([Y_p, Y_D, Y_He4, Y_Li7])
+        Y = np.asarray([Y_p, Y_D, Y_He3, Y_He4, Y_Li7])
 
         # Append processed mass fractions
         Y_model[:, i] = Y
 
     # Interpolate each function in logspace
     kind = "cubic"
-    Y_p, Y_D, Y_He4, Y_Li7 = Y_model
+    Y_p, Y_D, Y_He3, Y_He4, Y_Li7 = Y_model
 
     Y_p_interp = interp1d(np.log10(Omega_b0_vals), np.log10(Y_p), kind=kind)
     Y_D_interp = interp1d(np.log10(Omega_b0_vals), np.log10(Y_D), kind=kind)
+    Y_He3_interp = interp1d(np.log10(Omega_b0_vals), np.log10(Y_He3), kind=kind)
     Y_He4_interp = interp1d(np.log10(Omega_b0_vals), np.log10(Y_He4), kind=kind)
     Y_Li7_interp = interp1d(np.log10(Omega_b0_vals), np.log10(Y_Li7), kind=kind)
 
-    return Y_p_interp, Y_D_interp, Y_He4_interp, Y_Li7_interp
+    return Y_p_interp, Y_D_interp, Y_He3_interp, Y_He4_interp, Y_Li7_interp
 
 
 if __name__ == "__main__":
     # Variables
     T_i = 1e11  # initial temperature [K]
     T_f = 1e7  # final temperature [K]
-    y_min = (
-        0.5e-10  # minimum value for y-axis in the middle Y_i/Y_p relic abundance plot
-    )
+    y_min = 1e-10  # minimum value for y-axis in the middle Y_i/Y_p relic abundance plot
     Y_min = 1e-20  # lower bound value for mass fractions Y_i
     Omega_b0_min = 1e-2  # minimum value for Omega_b0
     Omega_b0_max = 1  # maximum value for Omega_b0
-    n = 4  # number of points for Omega_b0 before interpolation
+    n = 4  # number of points for Omega_b0 before interpolation TODO: RAISE
+
+    # Plotting variables
     n_plot = 1000  # number of points for Omega_b0 after interpolation
+    filename = os.path.join(FIG_DIR, "j_relic_abundances.png")  # plot filename
+    figsize = (7, 5)  # figure size
+
     Omega_b0_vals = np.logspace(
         np.log10(Omega_b0_min), np.log10(Omega_b0_max), n
-    )  # baryon density parameter today
-    filename = os.path.join(FIG_DIR, "j_relic_abundances.png")  # plot filename
-    figsize = (6, 5)  # figure size
+    )  # baryon density parameter today, array
 
     # Observed values for relic abundances Y_i/Y_p
     D_ab = 2.57e-5
@@ -134,19 +137,32 @@ if __name__ == "__main__":
     Li7_ab_err = 0.3e-10
 
     # Observed value for mass fraction 4Y_He4
-    mass_frac_He4 = 0.254
-    mass_frac_He4_err = 0.003
+    He4_mass_frac_obs = 0.254
+    He4_mass_frac_err = 0.003
 
     # Interpolate
-    Y_p_interp, Y_D_interp, Y_He4_interp, Y_Li7_interp = interpolate_Y_func_of_Omegab0(
-        Omega_b0_vals, N_species=8, Y_min=Y_min
-    )
+    (
+        Y_p_interp_func,
+        Y_D_interp_func,
+        Y_He3_interp_func,
+        Y_He4_interp_func,
+        Y_Li7_interp_func,
+    ) = interpolate_Y_func_of_Omegab0(Omega_b0_vals, N_species=8, Y_min=Y_min)
 
-    # Array to interpolate graph
+    # Array to interpolate graph with
     Omega_b0_arr = np.logspace(
         np.log10(Omega_b0_vals[0]), np.log10(Omega_b0_vals[-1]), n_plot
     )
     log_Omega_b0_arr = np.log10(Omega_b0_arr)
+
+    # Interpolate with these values
+    Y_p_interp = 10 ** (Y_p_interp_func(log_Omega_b0_arr))
+    Y_D_interp = 10 ** (Y_D_interp_func(log_Omega_b0_arr))
+    Y_He3_interp = 10 ** (Y_He3_interp_func(log_Omega_b0_arr))
+    Y_He4_interp = 10 ** (Y_He4_interp_func(log_Omega_b0_arr))
+    Y_Li7_interp = 10 ** (Y_Li7_interp_func(log_Omega_b0_arr))
+
+    He4_mass_frac_interp = 4 * Y_He4_interp
 
     # Plotting
     fig, axs = plt.subplots(
@@ -159,7 +175,7 @@ if __name__ == "__main__":
     # Plot 4Y_He4
     axs[0].plot(
         Omega_b0_arr,
-        4 * 10 ** (Y_He4_interp(log_Omega_b0_arr)),
+        He4_mass_frac_interp,
         label="4Y_He4",
         color=COLORS[5],
     )
@@ -168,8 +184,8 @@ if __name__ == "__main__":
     opacity = 0.3
     axs[0].fill_between(
         Omega_b0_arr,
-        mass_frac_He4 - mass_frac_He4_err,
-        mass_frac_He4 + mass_frac_He4_err,
+        He4_mass_frac_obs - He4_mass_frac_err,
+        He4_mass_frac_obs + He4_mass_frac_err,
         alpha=opacity,
         color=COLORS[5],
     )
@@ -181,18 +197,16 @@ if __name__ == "__main__":
     axs[0].grid(True)
 
     # Plot Y_i/Y_p for D, He3, and Li7
-    axs[1].loglog(
-        Omega_b0_arr, 10 ** (Y_D_interp(log_Omega_b0_arr)), label="D", color=COLORS[2]
-    )
+    axs[1].loglog(Omega_b0_arr, Y_D_interp / Y_p_interp, label="D", color=COLORS[2])
     axs[1].loglog(
         Omega_b0_arr,
-        10 ** (Y_He4_interp(log_Omega_b0_arr)),
+        Y_He3_interp / Y_p_interp,
         label="He3",
         color=COLORS[4],
     )
     axs[1].loglog(
         Omega_b0_arr,
-        10 ** (Y_Li7_interp(log_Omega_b0_arr)),
+        Y_Li7_interp / Y_p_interp,
         label="Li7",
         color=COLORS[-2],
     )
@@ -220,17 +234,45 @@ if __name__ == "__main__":
     axs[1].grid(True)
 
     # Calculate Bayesian likelihood
-    model = np.asarray(
-        [
+    """model = np.concatenate(
+        (
             10 ** (Y_D_interp(log_Omega_b0_arr)),
             10 ** (Y_He4_interp(log_Omega_b0_arr)),
             10 ** (Y_Li7_interp(log_Omega_b0_arr)),
-        ]
+        ),
+        axis=None,
     )
-    observed = np.asarray([D_ab, mass_frac_He4, Li7_ab])
-    error = np.asarray([D_ab_err, mass_frac_He4_err, Li7_ab_err])
 
-    likelihood = bayesian_probability(model, observed, error)
+    # Fill like-sized arrays with the observed and error scalar value
+    observed = np.asarray(
+        [
+            np.full(n_plot, D_ab),
+            np.full(n_plot, He4_mass_frac_obs),
+            np.full(n_plot, Li7_ab),
+        ]
+    ).flatten()
+    error = np.asarray(
+        [
+            np.full(n_plot, D_ab_err),
+            np.full(n_plot, He4_mass_frac_err),
+            np.full(n_plot, Li7_ab_err),
+        ]
+    ).flatten()"""
+    # Create likelihood as a function of Omega_b0, by using interpolated solutions
+    likelihood = [
+        bayesian_probability(
+            np.asarray(
+                [
+                    Y_D_interp[i],
+                    Y_Li7_interp[i],
+                    He4_mass_frac_interp[i],
+                ]
+            ),
+            np.asarray([D_ab, Li7_ab, He4_mass_frac_obs]),
+            np.asarray([D_ab_err, Li7_ab_err, He4_mass_frac_err]),
+        )
+        for i in range(n_plot)
+    ]
 
     # Plot Bayesian likelihood
     axs[2].plot(Omega_b0_arr, likelihood)