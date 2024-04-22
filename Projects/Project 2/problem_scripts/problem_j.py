75910a780f2778297d6814dc7dfa0c69472570d7
Rogumann <lasse.pladsen@hotmail.com>
2024-04-22 15:01:14 +0200

    changed interpolation from logspace base 10 to base e

--- Projects/Project 2/problem_scripts/problem_j.py
+++ Projects/Project 2/problem_scripts/problem_j.py
@@ -19,12 +19,8 @@ if __name__ == "__main__":
     # Variables
     T_i = 1e11  # initial temperature [K]
     T_f = 1e7  # final temperature [K]
-    n = 4  # number of points for Omega_b0 before interpolation TODO: RAISE
-    Omega_b0_min = 1e-2  # minimum value for Omega_b0
-    Omega_b0_max = 1  # maximum value for Omega_b0
+    Omega_b0_vals = np.logspace(-2, 0, 10)  # Omega_b0 values to test
     filename = FIG_DIR / "j_relic_abundances.png"
-
-    Omega_b0_vals = np.logspace(np.log10(Omega_b0_min), np.log10(Omega_b0_max), n)
 
     # Plot relic abundances
     BBN.plot_relic_abundances_Omegab0(T_i, T_f, Omega_b0_vals, filename=filename)
